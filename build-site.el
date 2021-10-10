09fa7aeb7d970d98bb24f69fc0e0797b8d3c9244
Gustavo de Paula <gustavodepaula@disroot.org>
2021-10-10 19:07:59 -0300

    use root instead of public

--- build-site.el
+++ build-site.el
@@ -27,7 +27,7 @@
              :recursive t
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./public"
+             :publishing-directory "./"
              :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc t                ;; Include a table of contents
