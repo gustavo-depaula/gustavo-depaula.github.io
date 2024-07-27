29808c209b454f3ce688929308f7b02f699d195c
Gustavo de Paula <dev@dpgu.me>
2024-07-27 00:53:38 -0300

    best bible study site ever

--- pages/catholicism/bible/bible.js
+++ pages/catholicism/bible/bible.js
@@ -1,8 +1,6 @@
 const daysList = document.getElementById("days-list");
 const daySections = document.getElementsByClassName("bible-day");
 
-console.log(daySections);
-
 [...daySections].forEach((daySection) => {
   const header = daySection.getElementsByTagName("h1")[0];
   header.id = header.innerText.toLowerCase().replace(/ /g, "-");
@@ -12,3 +10,32 @@ console.log(daySections);
 
   daysList.appendChild(element);
 });
+
+// const contentDiv = document.getElementsByTagName("main")[0];
+// const base_url = "https://www.biblegateway.com/passage/?search=";
+
+// const pattern = /\((\w+ \d+:\d+(-\d+)?)\)/g;
+
+// function createLink(match, p1) {
+//   const [bookChapter, verses] = p1.split(":");
+//   const link = `${base_url}${bookChapter}:${verses}&version=RSVCE`;
+//   return `<a href="${link}" target="_blank">(${p1})</a>`;
+// }
+
+// function replaceBibleReferences(element) {
+//   element.childNodes.forEach((node) => {
+//     if (node.nodeType === Node.TEXT_NODE) {
+//       const newText = node.textContent.replace(pattern, createLink);
+//       const tempDiv = document.createElement("div");
+//       tempDiv.innerHTML = newText;
+//       while (tempDiv.firstChild) {
+//         node.parentNode.insertBefore(tempDiv.firstChild, node);
+//       }
+//       node.parentNode.removeChild(node);
+//     } else if (node.nodeType === Node.ELEMENT_NODE) {
+//       replaceBibleReferences(node);
+//     }
+//   });
+// }
+
+// replaceBibleReferences(contentDiv);
