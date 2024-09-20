576b2c7d544564bfd849f6ab50ca69d7d4fd3a9c
Gustavo de Paula <dev@dpgu.me>
2024-09-20 11:28:52 -0300

    analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "cabin",
     },
     locale: "en-US",
     baseUrl: "dpgu.me",
